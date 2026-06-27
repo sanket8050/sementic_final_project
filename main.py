1645c54fc797aa1326cbe3e62829ebd218587e58
SANKET ADSARE <148283244+sanket8050@users.noreply.github.com>
2026-06-27 20:05:36 +0530

    Update main.py all origin

--- main.py
+++ main.py
@@ -989,9 +989,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
